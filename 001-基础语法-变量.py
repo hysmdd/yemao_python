50e8c89137e636e46e06e399379b8383449e5161
qinhao <coderqin@foxmail.com>
2024-04-08 16:39:19 +0800

    基本语法-变量

---
+++ 001-基础语法-变量.py
@@ -0,0 +1,29 @@
+a = 520
+b = 1314
+c = a + b
+d = '终会与你同行'
+e = d
+
+# 输出
+# print(339)
+# print(a)
+# print('终会与你同行')
+# print(d)
+# print(c)
+# print(e)
+# print("数字是：", c)
+# print("你的昵称是：", d)
+
+# 命名规范
+# 字母，数字，下划线，开头不能是数字
+# abc123_ = 3344
+# print(abc123_)
+# _abc123 = 3344
+# print(_abc123)
+
+# 简单易懂，见名知意
+name = "凌降"
+primary_name = "张哈子"
+primary_number = 20
+secondary_name = "洛小阳"
+secondary_number = 18
