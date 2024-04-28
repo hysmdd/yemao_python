9e59bc07a3bd6328c6d59dd73d0ac290b2e8ac44
qinhao <coderqin@foxmail.com>
2024-04-28 11:17:25 +0800

    selenium-防止检测-debugging模式

---
+++ 050-selenium-防止检测-2.py
@@ -0,0 +1,12 @@
+# 防止检测-debugging模式
+
+# 找到Chrome浏览器的安装路径
+# C:\Program Files\Google\Chrome\Application
+
+# 在命令行模式下输入下面的命令创建配置一个浏览器
+# chrome.exe --remote-debugging-port=8888 --user-data-dir="D:\data\chrome\profiles"
+
+# 在Chrome的快捷方式上点击，选择属性，快捷方式的目标栏后面加空格加上下面命令：
+# "C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=8888 --user-data-dir="D:\data\chrome\profiles"
+
+
